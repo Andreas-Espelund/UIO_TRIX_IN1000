069e6eec851d971532023c20d6a36746b84afee7
Andreas-Espelund <andreres@uio.no>
2020-10-11 22:20:06 +0200

    lagt til oppgave 09.04

---
+++ 09/04/dvd.py
@@ -0,0 +1,25 @@
+class Dvd: 
+    def __init__(self,tittel,produsent,utgaveNummer):
+        self._tittel = tittel
+        self._produsent = produsent 
+        self._utgaveNummer = utgaveNummer
+
+    def __str__(self):
+        return self._tittel
+
+    def __eq__(self,andre):
+        return (self._tittel == andre._tittel) and (self._produsent == andre._produsent) and (self._utgaveNummer == andre._utgaveNummer)
+
+'''
+TEST av Dvd klassen 
+'''
+
+minDvd = Dvd("starwars","disney","1st edition")
+
+annenDvd = Dvd("starwars","disney","1st edition")
+
+print(minDvd == annenDvd)
+
+print(minDvd)
+
+print(annenDvd)
